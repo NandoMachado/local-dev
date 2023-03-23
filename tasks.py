294e9c38cedc5246b4e2f42de6baccc3c1727fca
Nando Machado <nando.carnielmachado@sainsburys.co.uk>
2023-03-23 08:54:13 +0000

    Renaming

--- tasks.py
+++ tasks.py
@@ -2,5 +2,10 @@ from invoke import task
 
 
 @task
-def local_dev(c):
+def local_dev_up(c):
     c.run("docker-compose run -d local-setup")
+
+
+@task
+def local_dev_down(c):
+    c.run("docker-compose down -v")
